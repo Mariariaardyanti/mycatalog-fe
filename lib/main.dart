6559bdf4d6eb7242f5e874dfefd0cc4f095f3f7a
Mariariaardyanti <riaardyanti172gmail.com>
2026-04-22 10:51:14 +0700

    feat(core): setup MultiProvider dan integrasi AppRouter
    - Inisialisasi Firebase di main.dart
    - Menambahkan MultiProvider untuk AuthProvider dan ProductProvider
    - Mengintegrasikan AppRouter sebagai navigasi utama
    - Menentukan initial route ke halaman login
    - Persiapan state management global untuk auth dan catalog

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:flutter/material.dart';
-
+import 'package:shopping_app/features/auth/presentation/providers/auth_provider.dart';
+import 'package:shopping_app/features/catalog/presentation/providers/product_provider.dart';
+import 'package:shopping_app/core/routes/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inisialisasi Firebase SEBELUM runApp
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -15,8 +17,24 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()), //  penting
       ],
       child: const MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      // pakai router
+      initialRoute: AppRouter.login,
+      onGenerateRoute: AppRouter.onGenerateRoute,
+    );
+  }
 }
